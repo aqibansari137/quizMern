24909d0671903b929337139057e6a02bf5b69195
aqibansari137 <aqibansari137@gmail.com>
2022-04-17 15:11:40 +0530

    make it better

--- db.js
+++ db.js
@@ -3,8 +3,13 @@ const mongoose = require("mongoose");
 // const URL="mongodb+srv://quiz123:quiz123@cluster0.tszpy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const URL = process.env.URL;
 
-const connectDb = mongoose.connect(URL, () => {
-  console.log("Db connected");
-});
+mongoose
+  .connect(URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 module.exports = connectDb;
